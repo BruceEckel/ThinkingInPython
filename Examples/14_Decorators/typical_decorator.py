6612601eeb4fbe522bf52b1fd828cd7703d268dc
Bruce Eckel <mindviewinc@gmail.com>
2026-07-13 15:59:57 -0600

    Create typical_decorator.py

---
+++ Examples/14_Decorators/typical_decorator.py
@@ -0,0 +1,19 @@
+# typical_decorator.py
+from collections.abc import Callable
+
+def add_behavior(func: Callable) -> Callable:
+    def wrapper() -> None:
+        print("Some work")
+        func()
+        print("Some more work")
+
+    return wrapper
+
+@add_behavior
+def cheese() -> None:
+    print("Wensleydale")
+
+cheese()
+#: Some work
+#: Wensleydale
+#: Some more work
