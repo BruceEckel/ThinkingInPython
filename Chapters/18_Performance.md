9e49945645051856a297b4ac738bc1fd6f38f1d4
Bruce Eckel <mindviewinc@gmail.com>
2026-07-17 13:47:51 -0600

    18

--- Chapters/18_Performance.md
+++ Chapters/18_Performance.md
@@ -366,7 +366,7 @@ print(heap.pop(0))  # Smallest
 print(heap)  # 'heap[0]' no longer smallest
 #: [6, 4, 7, 10, 5, 8, 9]
 print(nsmallest(len(heap), heap))  # True smallest
-#: [4]
+#: [4, 5, 6, 7, 8, 9, 10]
 print(heap)  # Not reordered by nsmallest()
 #: [6, 4, 7, 10, 5, 8, 9]
 ```
--- Examples/18_Performance/heap_corruption.py
+++ Examples/18_Performance/heap_corruption.py
@@ -2,14 +2,14 @@
 from heapq import heapify, nsmallest
 
 heap = [10, 9, 8, 7, 6, 5, 4, 3]
-heapify(heap)              # In-place
+heapify(heap)  # In-place
 print(heap)
 #: [3, 6, 4, 7, 10, 5, 8, 9]
-print(heap.pop(0))         # Smallest
+print(heap.pop(0))  # Smallest
 #: 3
-print(heap)                # 'heap[0]' no longer smallest
+print(heap)  # 'heap[0]' no longer smallest
 #: [6, 4, 7, 10, 5, 8, 9]
-print(nsmallest(1, heap))  # The true smallest
-#: [4]
-print(heap)                # Not reordered by nsmallest()
-#: [4, 7, 10, 5, 8, 9]
+print(nsmallest(len(heap), heap))  # True smallest
+#: [4, 5, 6, 7, 8, 9, 10]
+print(heap)  # Not reordered by nsmallest()
+#: [6, 4, 7, 10, 5, 8, 9]
