a8851829969770b07edc4599cda9a1c8010114fb
Bruce Eckel <mindviewinc@gmail.com>
2026-07-26 15:48:49 -0600

    prune

--- Examples/24_Singleton/test_decorator.py
+++
@@ -1,5 +0,0 @@
-# test_decorator.py
-from singleton_class import Registry
-
-def test_decorator_returns_same_instance() -> None:
-    assert Registry("a") is Registry("b")
--- Examples/24_Singleton/test_singleton_class_variable.py
+++
@@ -1,8 +0,0 @@
-# test_singleton_class_variable.py
-from singleton_class_variable import CVSingleton
-
-def test_class_variable_returns_same_instance() -> None:
-    a = CVSingleton("a")
-    b = CVSingleton("b")
-    assert a is b
-    assert a.val == ["a", "b"]
